205c006dec4ba89bbe60bcb1e7eaa33cc75c7fd4
Fatima Ali <fatimaaliforwork@gmail.com>
2023-09-18 01:16:48 +0500

    updates cors options

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 const corsOptions = {
-	origin: "http://localhost:3000",
+	origin: "https://memories-frontend-mern.web.app",
 	credentials: true, //access-control-allow-credentials:true
 	optionSuccessStatus: 200,
 };
